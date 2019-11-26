05065ab240b35a6bfee989a077320a622aacecaa
Borisovsky Victor <vyuborisovskiy@stud.eltech.ru>
2019-11-26 17:01:33 +0300

    Func 2 first for all str

--- delete_odd_letter.c
+++ delete_odd_letter.c
@@ -7,35 +7,47 @@
 
 struct Text* delete_odd_letter(struct Text* text){
     char sep[2] = " ,";
-    int len_sent = strlen(text -> arr[0]);
-    char* buf = malloc(len_sent * sizeof(char));
-    strcpy(buf, text -> arr[0]);
-    char* istr;
-    istr = strtok(buf, sep);
-    int len_word = strlen(istr);
-    int odd = 0;
-    while(istr != NULL){
-        len_word = strlen(istr);
-        int count = 0;
-        for(int i = 0; i < len_word; i++){
-            if(isalpha(istr[i])){
-                count++;
+    int j = 0;
+    while(j < text -> n + 1) {
+        int len_sent = strlen(text->arr[j]);
+        char *buf = malloc(len_sent * sizeof(char));
+        strcpy(buf, text->arr[j]);
+        char *istr;
+        istr = strtok(buf, sep);
+        int len_word;
+        int odd = 0;
+        while (istr != NULL) {
+            len_word = strlen(istr);
+            int count = 0;
+            for (int i = 0; i < len_word; i++) {
+                if (isalpha(istr[i])) {
+                    count++;
+                }
             }
-        }
-        if(count % 2 == 0){
-            odd++;
-        }
-        istr = strtok(NULL, sep);
+            if (count % 2 == 0) {
+                odd++;
+            }
+            istr = strtok(NULL, sep);
 
 
-    }
-    if(odd == 0){
-        free(text -> arr[0]);
-        for (int i = 0; i < text -> n; i++){
-            text -> arr[i] = text-> arr [i + 1];
         }
-        text -> n--;
+        if (odd == 0) {
+            if (text -> arr[j][0] != ' '){
+                for (int i = 0; i < strlen(text -> arr[j + 1]); i++){
+                    text -> arr[j + 1][i] = text -> arr[j + 1][i + 1];
+                }
+            }
+            free(text->arr[j]);
+            for (int i = j; i < text-> n; i++) {
+                text->arr[i] = text->arr[i + 1];
+            }
+            text->n--;
+            free(buf);
+        }
+        else {
+            free(buf);
+            j++;
+        }
     }
-    free(buf);
     return text;
 };
